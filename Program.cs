6745c0c06067337bcdc3521a24d4e6faa924c309
fikri-kemal-adam <fb_kemal_1998_@hotmail.com>
2023-01-21 22:00:00 +0300

    for_example

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Loops;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please Enter a Number:");
+        int target = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= target; i++)
+        {
+            if(i%2==1)
+                Console.WriteLine(i);
+        }
+    }
+}
